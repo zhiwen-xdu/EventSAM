b575fc661f1b7bde8749bc8b96dbed81716848e1
EventLab@XDU <50076054+happychenpipi@users.noreply.github.com>
2024-04-08 16:08:27 +0800

    Update source_image_encoder.py

--- segment_anything/modeling/source_image_encoder.py
+++ segment_anything/modeling/source_image_encoder.py
@@ -14,7 +14,7 @@ from .common import LayerNorm2d, MLPBlock
 
 
 # This class and its supporting functions below lightly adapted from the ViTDet backbone available at: https://github.com/facebookresearch/detectron2/blob/main/detectron2/modeling/backbone/vit.py # noqa
-class ImageEncoderViT(nn.Module):
+class Source_ImageEncoderViT(nn.Module):
     def __init__(
         self,
         img_size: int = 1024,
